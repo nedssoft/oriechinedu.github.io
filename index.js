f4003762fb0678f47267c63cb2d686368bf130fe
Chinedu <oriechinedu@gmail.com>
2019-01-04 19:49:51 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,102 @@
+const getYear = () => {
+	const date = new Date();
+	return `Alright Reserved &copy; ${date.getFullYear()}`;
+}
+
+// const dateEl = document.getElementById('footer-content');
+// window.onload = () => dateEl.innerHTML = getYear();
+
+window.oncontextmenu = () => false;
+    
+$(document).ready(() => {
+  $('.wrapper').slick({
+	dots: true,
+	infinite: true,
+	slidesToShow: 1,
+	autoplay: true,
+		autoplaySpeed: 2000,
+		arrows: true,
+  });
+});
+
+
+const listenForScroll = e => {
+
+	const header   = document.getElementById('header');
+	const aboutMe  = document.getElementById('about-me');
+	const tools    = document.getElementById('tools');
+	const projects = document.getElementById('projects');
+	const home   = document.getElementById('home');
+	const aboutMeLi  = document.getElementById('about-me-li');
+	const toolsLi    = document.getElementById('tools-li');
+	const projectsLi    = document.getElementById('projects-li');
+	const hireMe = document.getElementById('hire-me');
+	const headerHeight =  getComputedStyle(header).height.split('px')[0];
+	const toolsHeight =  getComputedStyle(tools).height.split('px')[0];
+	const aboutMeHeight =  getComputedStyle(aboutMe).height.split('px')[0];
+	const projectsHeight =  getComputedStyle(projects).height.split('px')[0];
+	const active = 'active';
+
+
+	if (window.pageYOffset >= Number(headerHeight)){
+
+		home.classList.remove(active);
+	}
+	else {
+
+		home.classList.add(active);
+	}
+
+	let height1 = [Number(headerHeight), Number(aboutMeHeight)]
+
+	if (window.pageYOffset > headerHeight && window.pageYOffset < addHeight(height1)) {
+
+		aboutMeLi.classList.add(active);
+	}
+	else {
+		aboutMeLi.classList.remove(active);
+	}
+
+	let height2 = [Number(headerHeight), Number(aboutMeHeight), Number(toolsHeight)]
+
+	if (window.pageYOffset > addHeight(height1) && window.pageYOffset < addHeight(height2) ) {
+
+		toolsLi.classList.add(active);
+	}
+
+	else{
+		toolsLi.classList.remove(active);
+	}
+
+	let height3 = [Number(headerHeight), Number(aboutMeHeight), Number(toolsHeight), Number(projectsHeight)];
+
+	if (window.pageYOffset > addHeight(height2) && window.pageYOffset < addHeight(height3) ) {
+
+		projectsLi.classList.add(active);
+	}
+
+	else{
+		projectsLi.classList.remove(active);
+	}
+
+
+	if ((document.documentElement.scrollTop + window.innerHeight) >= document.documentElement.scrollHeight){
+		hireMe.classList.add(active);
+		projectsLi.classList.remove(active);
+	}
+	else{
+		hireMe.classList.remove(active);
+	}
+
+}
+
+const addHeight = heightArr => {
+
+	return heightArr.reduce((a,b) => a+b, 0);
+}
+
+
+window.addEventListener('scroll', listenForScroll );
+
+
+
