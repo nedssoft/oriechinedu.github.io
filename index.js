11aba3a5470c5c613bbf009e7221d8a697f3cf56
Chinedu <oriechinedu@gmail.com>
2019-01-08 20:15:42 +0100

    remove slick

--- index.js
+++ index.js
@@ -104,3 +104,32 @@ let navBarToggle = document.getElementById('js-navbar-toggle');
 navBarToggle.addEventListener('click', function () {
   mainNav.classList.toggle('show');
 });
+
+const slides = document.getElementsByClassName("mySlides");
+const dots = document.getElementsByClassName("dot");
+const showSlides = n => {
+	let i;
+	if (n > slides.length) {slideIndex = 1}    
+	if (n < 1) {slideIndex = slides.length}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";  
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" __active", "");
+	}
+	slides[slideIndex-1].style.display = "block";  
+	dots[slideIndex-1].className += " __active";
+  }
+let slideIndex = 1;
+showSlides(slideIndex);
+
+const plusSlides = n => {
+  showSlides(slideIndex += n);
+}
+
+const  currentSlide = n =>  {
+  showSlides(slideIndex = n);
+}
+
+
+
